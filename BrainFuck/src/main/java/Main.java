2f8e1fc29fe2a7cbd9174947de82ce0c5dbe928d
13thUnknown <misha34500@gmail.com>
2019-07-16 04:00:05 +0300

    Added logger

--- BrainFuck/src/main/java/Main.java
+++ BrainFuck/src/main/java/Main.java
@@ -1,12 +1,12 @@
+import lombok.extern.slf4j.Slf4j;
+import org.apache.log4j.BasicConfigurator;
 
+@Slf4j
 public class Main {
-
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         String fileName="C:\\BrainFuck.txt";
-        System.out.println("Application started!");
-        BrainFuckWorker worker=new BrainFuckWorker();
-        worker.start(fileName);
-
+        BasicConfigurator.configure();
+        log.info("Application started!");
+        BrainFuckWorker.start(fileName);
     }
 }
