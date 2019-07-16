5ef5ec139350051abe5bdc895c7635cd67802878
13thUnknown <misha34500@gmail.com>
2019-07-16 15:39:52 +0300

    Moved to new folder "Core"

---
+++ BrainFuck/src/main/java/Core/Lexer.java
@@ -0,0 +1,102 @@
+package Core;
+
+import static java.lang.System.exit;
+
+import Cell.Oper;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Lexer {
+  private static List<Oper> list;
+  public static List<Oper> analysis(String fileName) {
+    FileReader fileReader = openRead(fileName);
+    list=new ArrayList<Oper>();
+    int iteration=0;
+    int c;
+    int openClose=0;
+    try {
+      while ((c=fileReader.read()) != -1) {
+        switch ((char)c) {
+          case '>': {
+            list.add(new Oper(Oper.Type.FORWARD));
+            break;
+          }
+          case '<': {
+            list.add(new Oper(Oper.Type.BACK));
+            break;
+          }
+          case '+': {
+            list.add(new Oper(Oper.Type.ADD));
+            break;
+          }
+          case '-': {
+            list.add(new Oper(Oper.Type.MINUS));
+            break;
+          }
+          case '.': {
+            list.add(new Oper(Oper.Type.OUT));
+            break;
+          }
+          case ',': {
+            list.add(new Oper(Oper.Type.IN));
+            break;
+          }
+          case '[': {
+            list.add(new Oper(Oper.Type.WHILE));
+            openClose++;
+            break;
+          }
+          case ']': {
+            list.add(new Oper(Oper.Type.END));
+            openClose--;
+            if(openClose<0)
+            {
+              log.info("Lexical error: unpaired '[' and ']'.");
+              exit(4);
+            }
+            break;
+          }
+          default: {
+            log.info("Syntax error. Unknown symbol " + (char)c + " at iteration " + iteration);
+            exit(5);
+          }
+        }
+        iteration++;
+      }
+    }
+    catch (java.io.IOException e) {
+      log.info("There is exception: IOException");
+      log.error(e.toString());
+      exit(3);
+    }
+    if (openClose!=0) {
+      log.info("Lexical error: unpaired '[' and ']'.");
+      exit(4);
+    }
+    log.info("Lexical analysis is successful.");
+    try {
+      fileReader.close();
+    } catch (IOException e) {
+      log.info("There is exception: IOException");
+      log.error(e.toString());
+      exit(6);
+    }
+    return list;
+  }
+  static FileReader openRead(String fileName) {
+    try {
+      FileReader fileReader = new FileReader(fileName);
+      return fileReader;
+    }
+    catch (java.io.FileNotFoundException e) {
+      log.info("There is exception: FileNotFoundException");
+      log.error(e.toString());
+      exit(2);
+    }
+    return null;
+  }
+}
---
+++ BrainFuck/src/main/java/Core/Optimizer.java
@@ -0,0 +1,21 @@
+package Core;
+
+import Cell.Oper;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Optimizer {
+  public static void run(List<Oper> list) {
+    int i=0;
+    while ((i+1)<list.size()) {
+      if (list.get(i).getType()==list.get(i+1).getType()) {
+        list.get(i).times++;
+        list.remove(i+1);
+      }
+      else
+        i++;
+    }
+    log.info("Optimization successful.");
+  }
+}
